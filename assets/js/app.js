f37af97fc9e939013cd9adbfdcab1b54d8775ec5
Thomas Pourchel <tpourchel@auchan.fr>
2023-06-19 16:40:17 +0200

    modifs

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const url = "https://carambar-back.onrender.com/jokes"
+const question  = document.getElementById("question")
+const response  = document.getElementById("response")
+
+const random = fetch(url+ "/randomJoke").then(result => 
+  result.json()
+).then((random) => {
+  random.forEach(result => {
+    question.innerHTML += `${result.question}`
+  })
+})
